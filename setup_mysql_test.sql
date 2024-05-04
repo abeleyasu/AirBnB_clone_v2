1567a8ba7c5f72f9851a4655000f8046ffdf4b03
Abel Eyasu <abeleyasu98@gmail.com>
2024-05-04 06:46:00 -0700

    task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Task: 
+-- Script to setup MySQL for hbnb_test database
+
+-- Create hbnb_test_db if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create user hbnb_test if not exists
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to hbnb_test user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
